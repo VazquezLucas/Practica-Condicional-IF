6d6944d52eecef052eca00221f9e6994b834d904
Lucas Vazquez <vazquezlucasmatias2001@gmail.com>
2023-02-20 18:48:26 -0300

    Condicional IF
    20/02/2023

--- Program.cs
+++ Program.cs
@@ -7,6 +7,37 @@ namespace CondicionalIF
         static void Main(string[] args)
         {
 
+            //Declaracion de variables booleanas
+            bool haceFrio;
+
+            haceFrio= true;
+
+            Console.WriteLine(haceFrio);
+
+            //Negar una variable con el operador !
+            bool haceCalor;
+
+            haceCalor=true;
+
+            Console.WriteLine(!haceCalor);
+
+            //Condicional IF
+
+            int edad = 15;
+
+            Console.WriteLine("Vamos a evaluar si eres mayor de edad");
+
+            if (edad >= 18)
+            {
+
+                Console.WriteLine("Adelante puedes comprar porque eres mayor de edad");
+
+            }
+            else
+            {
+                Console.WriteLine("No puedes comprar porque eres menor de edad");
+            }
+
 
 
         }
